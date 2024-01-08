5ddbf9e640f64eb21218207ae4fdb60862cdf773
theac <theac@DESKTOP-LEARA2L>
2024-01-08 21:24:44 +0300

    Add project files.

---
+++ CalculateLength/Model/Pithagor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLength.Model
+{
+    static class  Pithagor
+    {
+        public static double CircleCircumference(double a) => 2 * Math.PI * a;
+    }
+}
---
+++ CalculateLength/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CalculateLength.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CalculateLength.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double _radius;
+
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _circumference;
+
+        public double Circumference
+        {
+            get => _circumference;
+
+            set
+            {
+                _circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Circumference = Pithagor.CircleCircumference(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+
+    }
+}
